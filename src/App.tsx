e37fd52c2c10d9dfe6738707d8f342366238f5e6
Irving Juárez <irvingjuarez274@gmail.com>
2022-06-12 23:55:37 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h2 className="text-3xl font-bold underline">Hello</h2>
+    </div>
+  )
+}
+
+export default App

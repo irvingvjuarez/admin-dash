c26672e2887b2faf48eb8191f3821f876917c16c
Irving Juárez <irvingjuarez274@gmail.com>
2022-06-23 12:57:43 -0500

    BugFix, slider on TodayTomorrowForecast already done

--- src/components/OneDayForecast.tsx
+++ src/components/OneDayForecast.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 interface OneDayForecastProps {
   data: any[],
   isSlider?: boolean,
@@ -5,10 +7,13 @@ interface OneDayForecastProps {
 }
 
 const OneDayForecast: React.FC<OneDayForecastProps> = ({ data, isSlider, isTransited }): JSX.Element => {
+  const [displayedData, setDisplayedData] = useState(data)
   const onOverlapping = (validation: boolean) => {
     // item.overlapping is true
     if(validation) return "text-primary"
   }
+  if(isTransited) setTimeout(() => setDisplayedData([]), 800)
+  if(displayedData.length === 0 && !isTransited) setDisplayedData(data)
 
   return(
     <div className={
@@ -16,7 +21,7 @@ const OneDayForecast: React.FC<OneDayForecastProps> = ({ data, isSlider, isTrans
         ${isSlider && "absolute transition-all duration-[800ms] ease-in-out"}
         ${isTransited && "moving"}`
       }>
-      {data.map(item => {
+      {displayedData.map(item => {
         const Component = item.statusIcon()
         return(
           <div key={item.id} className={item.overlapping ? "overlapped-day-forecast" : "standard-day-forecast"}>

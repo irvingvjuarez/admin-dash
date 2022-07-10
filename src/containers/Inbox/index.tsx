84bac041ede21305ea05b5d97804d36200c9ff80
Irving Juárez <irvingjuarez274@gmail.com>
2022-07-09 20:57:12 -0500

    A style issue already solved

--- src/containers/Inbox/index.tsx
+++ src/containers/Inbox/index.tsx
@@ -1,5 +1,7 @@
+import { useLocation } from "react-router-dom"
 import { ChatForm } from "../../components/ChatForm"
 import { ConsumerItem } from "../../components/ConsumerItem"
+import { MEDIA_QUERIES } from "../../globals"
 
 interface InboxProps {
   person?: any
@@ -10,8 +12,16 @@ const Inbox: React.FC<InboxProps> = ({
   person,
   containerClassName
 }) => {
+  const { pathname } = useLocation()
+  const getContainerClassName = () => {
+    let classname = "border border-[transparent]"
+    if(containerClassName) classname += ` ${containerClassName}`
+    if(window.screen.width >= MEDIA_QUERIES.md && /chat/.test(pathname) === false) classname += " border-l-just-white"
+    return classname
+  }
+
   return(
-    <section className={`border border-[transparent] ${containerClassName}`}>
+    <section className={getContainerClassName()}>
       {person ? (
         <>
           <div>

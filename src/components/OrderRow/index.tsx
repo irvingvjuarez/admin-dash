3f483a89ff98be4aaa9ee1176e999e48e179fba2
Irving Juárez <irvingjuarez274@gmail.com>
2022-07-17 16:43:34 -0500

    orders page completed

--- src/components/OrderRow/index.tsx
+++ src/components/OrderRow/index.tsx
@@ -1,11 +1,12 @@
-import { getContainerClassName, getCellClassName } from "./utils"
+import { getContainerClassName, getCellClassName, getStatusClassName } from "./utils"
+import { MdLocationOn } from "react-icons/md"
 
 interface OrderRowProps {
   id?: number
   item?: string
   customer?: string
   price?: number
-  status?: "Pending" | "Active" | "Rejected" | "Canceled"
+  status?: "Pending" | "Active" | "Rejected" | "Canceled" | "Complete"
   description?: string
   image?: string
   location?: string
@@ -23,16 +24,28 @@ const OrderRow: React.FC<OrderRowProps> = ({
   productID
 }): JSX.Element => {
   const columns = id ?  
-    [image, item, customer, price, status, productID, location] : 
+    [image, item, customer, "$" + price, status, productID, location] : 
     ["Image", "Item", "Customer", "Total Amount", "Status", "Order ID", "Location"]
 
   return(
     <div className={getContainerClassName(columns)}>
       {columns.map((column, index) => (
-        <div className={getCellClassName()} key={column}>
-          {(index === 0 && column !== "Image") ? (
-            <img src={image} key={column} width="90" />
-          ) : column}
+        <div key={column}>
+          {(index === 4 && column !== "Status") ? (
+            <span className={getStatusClassName(status)} >
+              {column}
+            </span>
+          ) : (
+            <div className={getCellClassName()} >
+              {(index === 6 && column !== "Location") && (
+                <MdLocationOn />
+              )}
+
+              {(index === 0 && column !== "Image") ? (
+                <img src={image} key={column} width="90" height="67.5px" />
+              ) : column}
+            </div>
+          )}
         </div>
       ))}
     </div>
--- src/components/OrderRow/utils.ts
+++ src/components/OrderRow/utils.ts
@@ -1,10 +1,20 @@
 export const getContainerClassName = (columns: any[]) => {
-  let classname = "w-full grid grid-cols-7 border-y-2 min-w-[900px] border-b-primary"
-  if(columns[0] === "Image") classname += " border-t-0"
+  let classname = "w-full grid grid-cols-7 place-items-center min-w-[900px] even:bg-primary-strong "
+  if(columns[0] === "Image") classname += " border-b-2"
   return classname
 }
 
 export const getCellClassName = () => {
   let classname = "tracking-wide py-2 md:text-lg flex justify-center items-center"
+  return classname
+}
+
+export const getStatusClassName = (status: "Pending" | "Active" | "Rejected" | "Canceled" | "Complete" | undefined) => {
+  let classname = "w-fit p-2 rounded-lg h-fit font-semibold tracking-wide mx-auto"
+  if(status === "Complete") classname += " bg-contrast-clear"
+  if(status === "Pending") classname += " bg-contrast2-clear"
+  if(status === "Rejected") classname += " bg-contrast3-clear"
+  if(status === "Active") classname += " bg-contrast4-clear"
+  if(status === "Canceled") classname += " bg-contrast-strong"
   return classname
 }

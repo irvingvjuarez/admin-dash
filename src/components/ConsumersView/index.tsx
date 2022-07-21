a70d10cee1b029ec26d91fc7f5732a1f4d12a86a
Irving Juárez <irvingjuarez274@gmail.com>
2022-07-20 23:09:23 -0500

    Changes in the style of the chat page

--- src/components/ConsumersView/index.tsx
+++ src/components/ConsumersView/index.tsx
@@ -3,7 +3,7 @@ import { ConsumerItem } from "../ConsumerItem"
 
 const ConsumersView = () => {
   return(
-    <article className="overflow-auto md:h-[83vh] lg:h-[85vh] xl:h-[87vh]">
+    <article className="overflow-auto md:h-[83vh] lg:h-[85vh] xl:h-[87vh] scrollbar-main">
       {consumers.map(consumer => 
         <ConsumerItem
           key={consumer.name}
